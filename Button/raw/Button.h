d435d15a302dd9b29319c2a70f2f6d840cd36df1
Sarmart <113354976+Samartic@users.noreply.github.com>
2023-07-18 18:46:01 -0400

    Update Button.h

--- Button/raw/Button.h
+++ Button/raw/Button.h
@@ -1,1 +1,21 @@
+#ifndef BUTTON_H
+#define BUTTON_H
 
+#include <Arduino.h>
+
+class Button{
+  private:
+    uint8_t pin;
+    int clicks;
+
+  public:
+    Button(uint8_t Dpin = -1);
+    void set(uint8_t Dpin);
+    void begin();
+    void update();
+    bool ispressed;
+    bool isengage;
+    int nbclicks;
+};
+
+#endif
