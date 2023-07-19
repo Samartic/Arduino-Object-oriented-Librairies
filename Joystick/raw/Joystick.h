d454bc676d8fe2df37995b71e1d43edf34b177c6
Sarmart <113354976+Samartic@users.noreply.github.com>
2023-07-18 21:00:41 -0400

    Update Joystick.h

--- Joystick/raw/Joystick.h
+++ Joystick/raw/Joystick.h
@@ -1,31 +1,12 @@
 #ifndef JOYSTICK_H
 #define JOYSTICK_H
-#include <
-#include <exception>
-
-class MissingLibraryException : public std::exception {
-private:
-    const char* libraryName;
-
-public:
-    MissingLibraryException(const char* name) : libraryName(name) {}
-
-    const char* what() const noexcept {
-        return libraryName;
-    }
-};
+#include <Potentiometer.h>
+#include <Button.h>
 
 class Joystick {
   private:
     Potentiometer x;
     Potentiometer y;
-    void checkLibraryAvailability(const char* libraryName) {
-    bool libraryAvailable = false;
-        if (!libraryAvailable) {
-            throw MissingLibraryException(libraryName);
-        }
-    }
-   
 
   public:
     Joystick(uint8_t Apinx = -1, uint8_t Apiny = -1, uint8_t Dpinsel = -1);
