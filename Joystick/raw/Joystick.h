cfa13a14fefe2712058a350753cb16a74427ca2c
Sarmart <113354976+Samartic@users.noreply.github.com>
2023-07-18 21:10:43 -0400

    Update Joystick.h

--- Joystick/raw/Joystick.h
+++ Joystick/raw/Joystick.h
@@ -1,21 +1,21 @@
 #ifndef JOYSTICK_H
 #define JOYSTICK_H
-#include <Potentiometer.h>
-#include <Button.h>
+#include "Potentiometer.h"
+#include "Button.h"
 
 class Joystick {
   private:
-    Potentiometer x;
-    Potentiometer y;
+      Potentiometer x;
+      Potentiometer y;
 
   public:
     Joystick(uint8_t Apinx = -1, uint8_t Apiny = -1, uint8_t Dpinsel = -1);
-    Button sel;
     void set(uint8_t Apinx, uint8_t Apiny, uint8_t Dpinsel);
     void begin();
     void update();
     float get_forces();
     float get_angle();
+    Button sel;
 };
 
 #endif
